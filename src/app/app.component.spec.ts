4432a10be6ef576c6ec174ceffabb33fa0c18fa6
Amar Salim <asabatheif@gmail.com>
2025-03-20 14:13:14 +0300

    added product-list component and product model

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,11 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'inventory-app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('inventory-app');
-  });
+
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
 import {  } from '@angular/router';
+import { Product } from './product.model';
+import { NgFor } from '@angular/common';
 
 @Component({
-  selector: 'inventory-app',
-  imports: [],
+  selector: 'inventory-app-root',
+  imports: [NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  products: Product[];
 
+  constructor(){
+    this.products = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/assets/images/products/black-shoes.jpg',
+        ['apparel', 'shoes', 'running'],
+        109.99),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/images/products/blue-jacket.jpg',
+        ['apparel', 'outerwear'],
+        238.99),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/assets/images/products/black-hat.jpg',
+        ['apparel', 'accessories'],
+        29.99)
+    ];  
+  }
+  productWasSelected(product: Product): void {
+    console.log('Product clicked: ', product); }
 }
