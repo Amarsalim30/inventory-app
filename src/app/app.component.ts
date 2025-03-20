ca2071ea7e312aac2005edc6b81fc91e809848f2
Amar Salim <asabatheif@gmail.com>
2025-03-20 13:55:44 +0300

    exmaple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {  } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'inventory-app',
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'inventory-app';
+
 }
